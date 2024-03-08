1d5064f67771a6c814088c73161f13faa82df200
Cameron Williams <cswilliams112004@gmail.com>
2024-03-08 15:54:16 -0500

    created the links and user models

---
+++ api/models/links.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import mapped_column
+from pydantic import BaseModel
+from models.base import Base
+from .users import User
+
+
+class Links(Base):
+    __tablename__ = "links"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    long_url = Column(String)
+    short_url = Column(String)
+    user_id = mapped_column(ForeignKey(User.id))
+
+class LinksSchema(BaseModel):
+    title: str
+    long_url: str
+    short_url: str
+    user_id: int
+
+class Config:
+    populate_by_name = True
